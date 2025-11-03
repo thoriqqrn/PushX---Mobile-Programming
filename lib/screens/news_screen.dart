288bf0b3df4bcee642d8a597857da49bfe76cf32
heyitskyy <zakyahmadsp4746@gmail.com>
2025-11-03 14:18:43 +0700

    Up

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -1,7 +1,6 @@
-// lib/screens/news_screen.dart (Premium Edition with Fun Facts)
-
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
+import '../utils/app_colors.dart';
 
 class NewsScreen extends StatefulWidget {
   const NewsScreen({super.key});
@@ -16,8 +15,9 @@ class _NewsScreenState extends State<NewsScreen>
   int _currentPage = 0;
   AnimationController? _iconController;
 
-  // Daftar fun facts tentang push up
-  final List<Map<String, dynamic>> funFacts = [
+  // Daftar fun facts yang LEBIH BANYAK (minimal 30+ untuk rotasi sebulan)
+  final List<Map<String, dynamic>> allFunFacts = [
+    // Week 1
     {
       'title': 'FUNFACT PUSH UP\nHARI INI !!!',
       'fact': 'PUSH UP\nMEREDAKAN\n150 PERSEN\nMASALAH\nHIDUP ANDA',
@@ -54,16 +54,180 @@ class _NewsScreenState extends State<NewsScreen>
       'icon': Icons.accessibility_new_rounded,
       'gradient': [const Color(0xFF43e97b), const Color(0xFF38f9d7)],
     },
+    
+    // Week 2
+    {
+      'title': 'DID YOU KNOW?',
+      'fact': 'PUSH UP\nMEMBANTU\nMENGURANGI\nSTRESS & ANXIETY',
+      'icon': Icons.mood_rounded,
+      'gradient': [const Color(0xFFa8edea), const Color(0xFFfed6e3)],
+    },
+    {
+      'title': 'FUN FACT!',
+      'fact': 'OTOT DADA\nMULAI TERBENTUK\nSETELAH 2 MINGGU',
+      'icon': Icons.timer_rounded,
+      'gradient': [const Color(0xFFffecd2), const Color(0xFFfcb69f)],
+    },
+    {
+      'title': 'INCREDIBLE!',
+      'fact': 'PUSH UP\nMENINGKATKAN\nMETABOLISME\n25 PERSEN',
+      'icon': Icons.bolt_rounded,
+      'gradient': [const Color(0xFFff9a9e), const Color(0xFFfecfef)],
+    },
+    {
+      'title': 'KESEHATAN!',
+      'fact': 'PUSH UP\nMENGURANGI\nRISIKO\nOSTEOPOROSIS',
+      'icon': Icons.healing_rounded,
+      'gradient': [const Color(0xFFfbc2eb), const Color(0xFFa6c1ee)],
+    },
+    {
+      'title': 'MOTIVASI!',
+      'fact': 'KONSISTENSI\n3 BULAN\nCUKUP UNTUK\nPERUBAHAN BESAR',
+      'icon': Icons.trending_up_rounded,
+      'gradient': [const Color(0xFFfdcbf1), const Color(0xFFe6dee9)],
+    },
+    {
+      'title': 'SCIENCE!',
+      'fact': 'PUSH UP\nMERILIS\nENDORPHIN\nHORMON BAHAGIA',
+      'icon': Icons.psychology_rounded,
+      'gradient': [const Color(0xFFa1c4fd), const Color(0xFFc2e9fb)],
+    },
+
+    // Week 3
+    {
+      'title': 'POWER UP!',
+      'fact': 'DIAMOND\nPUSH UP\nPALING EFEKTIF\nUNTUK TRICEPS',
+      'icon': Icons.star_rounded,
+      'gradient': [const Color(0xFFd299c2), const Color(0xFFfef9d7)],
+    },
+    {
+      'title': 'BOOST!',
+      'fact': 'PUSH UP\nMEMPERCEPAT\nALIRAN DARAH\n30 PERSEN',
+      'icon': Icons.water_drop_rounded,
+      'gradient': [const Color(0xFFfad0c4), const Color(0xFFffd1ff)],
+    },
+    {
+      'title': 'STRONG!',
+      'fact': 'PLYOMETRIC\nPUSH UP\nTINGKATKAN\nPOWER LEDAKAN',
+      'icon': Icons.flashlight_on_rounded,
+      'gradient': [const Color(0xFFffeaa7), const Color(0xFFfdcb6e)],
+    },
+    {
+      'title': 'ENERGY!',
+      'fact': 'PUSH UP PAGI\nMENINGKATKAN\nENERGI HARIAN\n50 PERSEN',
+      'icon': Icons.wb_sunny_rounded,
+      'gradient': [const Color(0xFFf6d365), const Color(0xFFfda085)],
+    },
+    {
+      'title': 'RECOVERY!',
+      'fact': 'ISTIRAHAT\n48 JAM\nPENTING UNTUK\nPERTUMBUHAN OTOT',
+      'icon': Icons.bed_rounded,
+      'gradient': [const Color(0xFFd7d2cc), const Color(0xFF304352)],
+    },
+    {
+      'title': 'BALANCE!',
+      'fact': 'PUSH UP\nMELATIH\nKESTABILAN\nCORE MUSCLES',
+      'icon': Icons.balance_rounded,
+      'gradient': [const Color(0xFFe0c3fc), const Color(0xFF8ec5fc)],
+    },
+
+    // Week 4
+    {
+      'title': 'TECHNIQUE!',
+      'fact': 'WIDE PUSH UP\nFOKUS PADA\nOTOT DADA\nBAGIAN LUAR',
+      'icon': Icons.open_in_full_rounded,
+      'gradient': [const Color(0xFFf093fb), const Color(0xFFf5576c)],
+    },
+    {
+      'title': 'PROGRESS!',
+      'fact': 'TAMBAH 1\nPUSH UP\nTIAP HARI\n= 365 SETAHUN',
+      'icon': Icons.calendar_today_rounded,
+      'gradient': [const Color(0xFF4facfe), const Color(0xFF00f2fe)],
+    },
+    {
+      'title': 'MINDSET!',
+      'fact': 'MENTAL\nSAMA PENTING\nDENGAN FISIK\nDALAM TRAINING',
+      'icon': Icons.lightbulb_rounded,
+      'gradient': [const Color(0xFF43e97b), const Color(0xFF38f9d7)],
+    },
+    {
+      'title': 'NUTRITION!',
+      'fact': 'PROTEIN\n20-30 GRAM\nPER MEAL\nOPTIMAL UNTUK\nOTOT',
+      'icon': Icons.restaurant_rounded,
+      'gradient': [const Color(0xFFfa709a), const Color(0xFFfee140)],
+    },
+    {
+      'title': 'HYDRATION!',
+      'fact': 'MINUM AIR\n2-3 LITER\nPER HARI\nMAKSIMALKAN\nPERFORMANCE',
+      'icon': Icons.local_drink_rounded,
+      'gradient': [const Color(0xFF667eea), const Color(0xFF764ba2)],
+    },
+    {
+      'title': 'CONSISTENCY!',
+      'fact': 'LATIHAN RUTIN\n3-4 KALI\nSEMINGGU\nKUNCI KESUKSESAN',
+      'icon': Icons.verified_rounded,
+      'gradient': [const Color(0xFFff6b6b), const Color(0xFFffa502)],
+    },
+
+    // Bonus Facts
+    {
+      'title': 'LEGENDARY!',
+      'fact': 'JACK LALANNE\nDI USIA 70\nMASIH BISA\n1,000 PUSH UP',
+      'icon': Icons.elderly_rounded,
+      'gradient': [const Color(0xFFfbc2eb), const Color(0xFFa6c1ee)],
+    },
+    {
+      'title': 'MILITARY!',
+      'fact': 'STANDAR US ARMY\nMINIMAL 42\nPUSH UP\nDALAM 2 MENIT',
+      'icon': Icons.shield_rounded,
+      'gradient': [const Color(0xFFa8edea), const Color(0xFFfed6e3)],
+    },
+    {
+      'title': 'WORLD CLASS!',
+      'fact': 'ATLET OLYMPIC\nBISA 200+\nPUSH UP\nTANPA HENTI',
+      'icon': Icons.workspace_premium_rounded,
+      'gradient': [const Color(0xFFffecd2), const Color(0xFFfcb69f)],
+    },
+    {
+      'title': 'TRANSFORMATION!',
+      'fact': '100 PUSH UP\nSETIAP HARI\n= BODY GOALS\nDALAM 6 BULAN',
+      'icon': Icons.auto_awesome_rounded,
+      'gradient': [const Color(0xFFd299c2), const Color(0xFFfef9d7)],
+    },
   ];
+
+  // Fungsi untuk get daily fun facts (6 facts per hari)
+  List<Map<String, dynamic>> getDailyFunFacts() {
+    final today = DateTime.now();
+    final dayOfYear = today.difference(DateTime(today.year, 1, 1)).inDays;
+    
+    // Shuffle based on day of year
+    final shuffledFacts = List<Map<String, dynamic>>.from(allFunFacts);
+    final seed = dayOfYear;
+    final random = math.Random(seed);
+    
+    shuffledFacts.shuffle(random);
+    
+    // Return first 6 facts
+    return shuffledFacts.take(6).toList();
+  }
+
+  late List<Map<String, dynamic>> funFacts;
 
   @override
   void initState() {
     super.initState();
+    
+    // Load daily fun facts
+    funFacts = getDailyFunFacts();
+    
     _iconController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 1500),
     )..repeat(reverse: true);
   }
+
+  // ... rest of the code sama
 
   @override
   void dispose() {
@@ -75,7 +239,7 @@ class _NewsScreenState extends State<NewsScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: AppColors.background(context),
       body: SafeArea(
         child: Column(
           children: [
@@ -85,12 +249,12 @@ class _NewsScreenState extends State<NewsScreen>
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Text(
+                  Text(
                     'NEWS',
                     style: TextStyle(
                       fontSize: 16,
                       fontWeight: FontWeight.w700,
-                      color: Colors.grey,
+                      color: AppColors.secondaryText(context),
                       letterSpacing: 2,
                     ),
                   ),
@@ -100,14 +264,17 @@ class _NewsScreenState extends State<NewsScreen>
                       vertical: 6,
                     ),
                     decoration: BoxDecoration(
-                      color: Colors.grey[900],
+                      color: AppColors.cardBackground(context),
                       borderRadius: BorderRadius.circular(20),
-                      border: Border.all(color: Colors.grey[800]!, width: 1),
+                      border: Border.all(
+                        color: AppColors.cardBorder(context),
+                        width: 1,
+                      ),
                     ),
                     child: Text(
                       '${_currentPage + 1}/${funFacts.length}',
                       style: TextStyle(
-                        color: Colors.grey[400],
+                        color: AppColors.secondaryText(context),
                         fontSize: 12,
                         fontWeight: FontWeight.w700,
                       ),
@@ -157,6 +324,7 @@ class _NewsScreenState extends State<NewsScreen>
                 children: [
                   // Previous Button
                   _buildNavButton(
+                    context,
                     icon: Icons.chevron_left_rounded,
                     onTap: () {
                       if (_currentPage > 0) {
@@ -182,8 +350,8 @@ class _NewsScreenState extends State<NewsScreen>
                         height: 8,
                         decoration: BoxDecoration(
                           color: _currentPage == index
-                              ? Colors.white
-                              : Colors.grey[800],
+                              ? AppColors.primaryText(context)
+                              : AppColors.cardBorder(context),
                           borderRadius: BorderRadius.circular(4),
                         ),
                       ),
@@ -194,6 +362,7 @@ class _NewsScreenState extends State<NewsScreen>
 
                   // Next Button
                   _buildNavButton(
+                    context,
                     icon: Icons.chevron_right_rounded,
                     onTap: () {
                       if (_currentPage < funFacts.length - 1) {
@@ -243,7 +412,13 @@ class _NewsScreenState extends State<NewsScreen>
         child: Stack(
           children: [
             // Background pattern
-            Positioned.fill(child: CustomPaint(painter: PatternPainter())),
+            Positioned.fill(
+              child: CustomPaint(
+                painter: PatternPainter(
+                  isDark: AppColors.isDark(context),
+                ),
+              ),
+            ),
 
             // Content
             SingleChildScrollView(
@@ -325,26 +500,35 @@ class _NewsScreenState extends State<NewsScreen>
     );
   }
 
-  Widget _buildNavButton({
+  Widget _buildNavButton(
+    BuildContext context, {
     required IconData icon,
     required VoidCallback onTap,
     required bool enabled,
   }) {
+    final isDark = AppColors.isDark(context);
+    
     return GestureDetector(
       onTap: enabled ? onTap : null,
       child: Container(
         padding: const EdgeInsets.all(12),
         decoration: BoxDecoration(
-          color: enabled ? Colors.white : Colors.grey[900],
+          color: enabled 
+              ? (isDark ? Colors.white : Colors.black)
+              : AppColors.cardBackground(context),
           shape: BoxShape.circle,
           border: Border.all(
-            color: enabled ? Colors.white : Colors.grey[800]!,
+            color: enabled 
+                ? (isDark ? Colors.white : Colors.black)
+                : AppColors.cardBorder(context),
             width: 2,
           ),
           boxShadow: enabled
               ? [
                   BoxShadow(
-                    color: Colors.white.withOpacity(0.3),
+                    color: isDark
+                        ? Colors.white.withOpacity(0.3)
+                        : Colors.black.withOpacity(0.2),
                     blurRadius: 15,
                     spreadRadius: 2,
                   ),
@@ -353,7 +537,9 @@ class _NewsScreenState extends State<NewsScreen>
         ),
         child: Icon(
           icon,
-          color: enabled ? Colors.black : Colors.grey[700],
+          color: enabled 
+              ? (isDark ? Colors.black : Colors.white)
+              : AppColors.tertiaryText(context),
           size: 24,
         ),
       ),
@@ -363,6 +549,10 @@ class _NewsScreenState extends State<NewsScreen>
 
 // Custom painter untuk background pattern
 class PatternPainter extends CustomPainter {
+  final bool isDark;
+
+  PatternPainter({required this.isDark});
+
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
@@ -386,5 +576,7 @@ class PatternPainter extends CustomPainter {
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+  bool shouldRepaint(covariant PatternPainter oldDelegate) {
+    return oldDelegate.isDark != isDark;
+  }
 }
